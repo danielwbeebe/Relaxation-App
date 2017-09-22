2f5f52b0b3ce323b3b7ee3bac61b493d8b3eb812
Daniel Beebe <danwbeebe@gmail.com>
2017-09-22 16:24:29 -0400

    setup header, main, footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+        <div className="header">
+          Header
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="main">
+          Main
+        </div>
+
+        <div className="footer">
+          Footer
+        </div>
+
       </div>
     );
   }
