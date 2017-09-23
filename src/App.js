4ed3f7b61f49f35f3db2f08dbe6b4875b8c840a2
Daniel Beebe <danwbeebe@gmail.com>
2017-09-22 23:13:04 -0400

    add welcome component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-        <div className="header">
-          Header
-        </div>
-
-        <div className="main">
-          Main
-        </div>
-
-        <div className="footer">
-          Footer
-        </div>
-
-      </div>
+      <Router>
+        <Route exact path="/" render={() => <Welcome />} />
+      </Router>
     );
   }
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Welcome = () => {
+
+  return (
+    <div className="welcome">
+
+      <div className="header">
+        Header
+      </div>
+
+      <div className="main">
+        Main
+      </div>
+
+      <div className="footer">
+        Footer
+      </div>
+
+    </div>
+  )
+}
+
+export default Welcome;
