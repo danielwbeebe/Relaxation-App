4a880923d7c3992309d166b60d8e500326dc60c3
Daniel Beebe <danwbeebe@gmail.com>
2017-10-09 15:53:56 -0400

    play bird sound when enter garden page

--- src/components/Garden.jsx
+++ src/components/Garden.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-const Garden = (props) => {
+import birdSound from './sounds/garden-bird-sound.wav';
 
-  return (
-    <div className="garden">
+class Garden extends Component {
+  constructor(){
+    super();
+      this.state = {
+        sound: false,
+      }
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
 
-      <div className="header">
-        <Link to="/home"><h1 className="header-link">Home</h1></Link>
-        <button className="timer-button"><h4>3-Minute Timer</h4></button>
+  componentDidMount() {
+    this.playSound();
+  }
+
+  playSound() {
+    // free bird sound from klankbeeld at https://freesound.org/people/klankbeeld/sounds/187829/
+    var audio = new Audio(birdSound);
+    audio.play();
+  }
+
+  pauseSound() {
+    var audio = new Audio(birdSound);
+    audio.pause();
+  }
+
+  render(){
+    return (
+      <div className="garden">
+
+        <div className="header">
+          <Link to="/home" onClick="pauseSound"><h1 className="header-link">Home</h1></Link>
+          <button className="timer-button"><h4>3-Minute Timer</h4></button>
+        </div>
+
+        <div className="main">
+
+        </div>
+
+        <div className="footer">
+          <a href="https://github.com/danielwbeebe" target="_blank" rel="noopener noreferrer"><h2>Created By Dan Beebe</h2></a>
+        </div>
+
       </div>
-
-      <div className="main">
-
-      </div>
-
-      <div className="footer">
-        <a href="https://github.com/danielwbeebe" target="_blank" rel="noopener noreferrer"><h2>Created By Dan Beebe</h2></a>
-      </div>
-
-    </div>
-  )
+    )
+  }
 }
 
 export default Garden;
