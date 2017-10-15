f4a48bdf6449b0ef3020db3622c3bb2dd9a0e885
Daniel Beebe <danwbeebe@gmail.com>
2017-10-14 22:21:14 -0400

    remove code for timer, errors not fixed

--- src/components/Garden.jsx
+++ src/components/Garden.jsx
@@ -5,58 +5,35 @@ import birdSound from './sounds/garden-bird-sound.wav';
 
 // free bird sound from klankbeeld at https://freesound.org/people/klankbeeld/sounds/187829/
 var audio = new Audio(birdSound);
-var seconds = 0;
 
 class Garden extends Component {
   constructor(){
     super();
       this.state = {
         button: "Listen",
-        clock: "none",
       }
     this.componentDidMount = this.componentDidMount.bind(this);
     this.playSound = this.playSound.bind(this);
-    this.clockOn = this.clockOn.bind(this);
-    this.addSeconds = this.addSeconds.bind(this);
   }
 
   componentDidMount() {
-    document.getElementById("clock").style.display = this.state.clock;
   }
 
   playSound() {
     if (this.state.button === "Listen") {
       this.setState({
       button: "Stop",
-      clock: "inline-block",
       })
     audio.loop = true;
     audio.play();
-
-      this.clockOn();
-    } else {
+    }
+    else {
       clearInterval(this.timer);
       this.setState({
         button: "Listen",
       })
       audio.pause();
-      document.getElementById("clock").style.display = "none";
     }
-  }
-
-  clockOn() {
-    var timer = setInterval(this.addSeconds, 1000);
-    this.addSeconds();
-  }
-
-  addSeconds() {
-    seconds = seconds + 1;
-    document.getElementById("clock").innerHTML = seconds + " seconds";
-    document.getElementById("clock").style.display = "inline-block";
-  }
-
-  stopSound() {
-    audio.pause();
   }
 
   render(){
@@ -64,13 +41,13 @@ class Garden extends Component {
       <div className="garden">
 
         <div className="header">
-          <Link to="/home" onClick={this.stopSound}><h1 className="header-link">Home</h1></Link>
+          <Link to="/home" onClick={this.playSound}><h1 className="header-link">Home</h1></Link>
           <button className="timer-button" onClick={this.playSound}><h4>{this.state.button}</h4></button>
         </div>
 
         <div className="main">
           <div id="clock">
-            <h1>CLOCK</h1>
+
           </div>
         </div>
 
