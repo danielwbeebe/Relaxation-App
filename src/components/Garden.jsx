2cfb7ce3066e34d3f05fb8bbeeac63724544a89f
Daniel Beebe <danwbeebe@gmail.com>
2017-10-25 21:33:02 -0400

    add 5 minute timer

--- src/components/Garden.jsx
+++ src/components/Garden.jsx
@@ -12,28 +12,35 @@ class Garden extends Component {
       this.state = {
         button: "1 Min",
         button3: "3 Min",
+        button5: "5 Min",
         seconds: 60,
         secondsthree: 180,
+        secondsfive: 300,
         clock: null,
       }
     this.playSound = this.playSound.bind(this);
     this.playSoundThree = this.playSoundThree.bind(this);
+    this.playSoundFive = this.playSoundFive.bind(this);
     this.stopSound = this.stopSound.bind(this);
     this.startTimer = this.startTimer.bind(this);
     this.startTimerThree = this.startTimerThree.bind(this);
+    this.startTimerFive = this.startTimerFive.bind(this);
     this.countSeconds = this.countSeconds.bind(this);
     this.countSecondsThree = this.countSecondsThree.bind(this);
+    this.countSecondsFive = this.countSecondsFive.bind(this);
   }
 
   componentDidMount() {
     document.getElementById("clock").style.display = "none";
     document.getElementById("clock3").style.display = "none";
+    document.getElementById("clock5").style.display = "none";
   }
 
   playSound() {
     this.setState({
     button: "",
     button3: "",
+    button5: "",
     })
     audio.loop = true;
     audio.play();
@@ -44,10 +51,22 @@ class Garden extends Component {
     this.setState({
     button: "",
     button3: "",
+    button5: "",
     })
     audio.loop = true;
     audio.play();
     this.startTimerThree();
+  }
+
+  playSoundFive() {
+    this.setState({
+    button: "",
+    button3: "",
+    button5: "",
+    })
+    audio.loop = true;
+    audio.play();
+    this.startTimerFive();
   }
 
   stopSound() {
@@ -57,6 +76,7 @@ class Garden extends Component {
   startTimer() {
     document.getElementById("timer-button").style.display = "none";
     document.getElementById("timer-button3").style.display = "none";
+    document.getElementById("timer-button5").style.display = "none";
     let clock = setInterval(this.countSeconds, 1000);
     document.getElementById("clock").style.display = "inline-block";
   }
@@ -64,8 +84,17 @@ class Garden extends Component {
   startTimerThree() {
     document.getElementById("timer-button").style.display = "none";
     document.getElementById("timer-button3").style.display = "none";
+    document.getElementById("timer-button5").style.display = "none";
     let clock = setInterval(this.countSecondsThree, 1000);
     document.getElementById("clock3").style.display = "inline-block";
+  }
+
+  startTimerFive() {
+    document.getElementById("timer-button").style.display = "none";
+    document.getElementById("timer-button3").style.display = "none";
+    document.getElementById("timer-button5").style.display = "none";
+    let clock = setInterval(this.countSecondsFive, 1000);
+    document.getElementById("clock5").style.display = "inline-block";
   }
 
   countSeconds() {
@@ -94,6 +123,19 @@ class Garden extends Component {
     }
   }
 
+  countSecondsFive() {
+    if (this.state.secondsfive > 0) {
+      this.setState({
+        secondsfive: this.state.secondsfive -1,
+      })
+    } if (this.state.secondsfive <= 0) {
+      audio.pause();
+      this.setState({
+        secondsfive: "Done",
+      })
+    }
+  }
+
   render(){
     return (
       <div className="garden">
@@ -102,6 +144,7 @@ class Garden extends Component {
           <Link to="/home" onClick={this.stopSound}><h1 className="header-link">Home</h1></Link>
           <button className="timer-button" id="timer-button" onClick={this.playSound}><h4>{this.state.button}</h4></button>
           <button className="timer-button" id="timer-button3" onClick={this.playSoundThree}><h4>{this.state.button3}</h4></button>
+          <button className="timer-button" id="timer-button5" onClick={this.playSoundFive}><h4>{this.state.button5}</h4></button>
         </div>
 
         <div className="main">
@@ -110,6 +153,9 @@ class Garden extends Component {
           </div>
           <div id="clock3">
             {this.state.secondsthree}
+          </div>
+          <div id="clock5">
+            {this.state.secondsfive}
           </div>
         </div>
 
