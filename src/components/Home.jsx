205cc832822763a9bea1a2664e9f7c92f511c871
Daniel Beebe <danwbeebe@gmail.com>
2017-10-28 13:52:47 -0400

    add winter page link to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,6 +35,9 @@ class Home extends Component {
           </div>
 
           <div className="main-lower">
+            <div className="enter-winter">
+              <Link to="/winter"><h3 className="home-link">Winter In The Park</h3><img src='./images/welcome-winter.png' alt="Copyright 2015 By Dan Beebe"></img></Link>
+            </div>
             <div className="enter-garden">
               <Link to="/garden"><h3 className="home-link">Spring In The Garden</h3><img src='./images/welcome-boston.png' alt="Copyright 2014 By Dan Beebe"></img></Link>
             </div>
